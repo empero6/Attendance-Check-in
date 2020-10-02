b5b95eef84a5b3569c30dfa22a607f899b8e23eb
empero6 <empero6@gmail.com>
2020-10-01 21:21:59 -0400

    Add files via upload

---
+++ src/components/navbar.component.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">ISSS Attendance</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link"> View Events</Link>
+          </li>
+          <li className = "navbar-item">
+            <Link to = "/create" className = "nav-link">Create Attendance Log </Link>
+          </li>
+          <li className = "navbar-item">
+            <Link to = "/add" className = "nav-link"> Create Event </Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create User</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
